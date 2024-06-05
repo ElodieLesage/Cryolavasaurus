786929320ebb098ab19fe3a56851cabb02c42f95
Elodie Lesage <48713677+ElodieLesage@users.noreply.github.com>
2024-06-05 15:46:44 -0700

    Add files via upload

---
+++ Input/PROD_MCC/PROD_MCC_22.m
@@ -0,0 +1,122 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Multiphase 1-dimensional Satellite Thermal Solver
+% Spherically symmetric solver for the heat diffusion equation, explicit
+% finite difference in time. Tailored for applications to planetary
+% evolution with considerations for phase change, reservoir freezing, and 
+% eruption.
+% Sam Howell, Elodie Lesage, Julia Miller
+% (C)2022 California Institute of Technology. All rights reserved.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% clear; clc;
+
+%%%%% TBD:
+% Porosity
+%%%%%
+
+IN.namePrefix = 'PROD_MCC_22';
+
+%%%%%%%%%%%%%%%%%%
+% Model Timing
+%%%%%%%%%%%%%%%%%%
+
+IN.tRes  = '5 Myr'; % Time after which to start emplacement
+IN.tMax  = '5 Myr'; % Model run time after emplacement, use 'yr' 'kyr' 'Myr' or 'Gyr'
+
+% Output Frequency 
+IN.tOut = '0.5 kyr'; % Results (IN.tMult times slower w/ no res)
+IN.tMult = 20;
+
+IN.tOut2 = '0.5 kyr'; % Results (IN.tMult times slower w/ no res)
+IN.tMult2 = 20;
+
+IN.pltOn = 0;        % Plot output in real time
+IN.movOn = 1;        % Save plot movie
+IN.convection = 1;   % Turn convection on or off
+
+%%%%%%%%%%%%%%%%%%
+% Body Settings
+%%%%%%%%%%%%%%%%%%
+IN.body = 'Europa';   % Target of interest
+IN.lat  = 0;          % Latitude of simulation (deg)
+IN.tides = 1;         % Tidal heating (0 = off, 1 = on)
+
+%%%%%%%%%%%%%%%%%%
+% Geometry
+%%%%%%%%%%%%%%%%%%
+IN.H0  = 23.8e3;   % Initial ice shell thickness [m]
+IN.setTargetThickness = 0;   % 0 - specify radiogenic only (BOD.Qrad), 1 - Specifiy desired thickness IN.H0
+IN.Nz  = 300;      % Number of nodes in vertical  direction
+IN.Nres = 20;      % Minimum number of resevoir nodes
+
+% Geometry
+IN.rRes = 2500;
+IN.zResTop = 2500;
+
+%%%%%%%%%%%%%%%%%%
+% Thermal Properties
+%%%%%%%%%%%%%%%%%%
+% IN.Tm_ocn = 273;    % Melting temp of ocean [K] - NOTE: This should set 
+% equal to the initial reservoir melting temperature 
+IN.CpOcn  = 4184;     % Specific heat capacity [J/kg K]
+MAT.L     = 330e3;    % Latent heat of fusion [kJ/kg]
+IN.X      = 5e-10;    % Liquid water compressibility [Pa^-1]
+
+%%%%%%%%%%%%%%%%%%
+% Composition
+%%%%%%%%%%%%%%%%%%
+% 1  = Eq EM-CM carb
+% 3  = Eq EM-CI carb
+% 8  = Eq MC-Scale carb + comets
+% 17 = Pure Water
+IN.simu   = 8;       % Simulation number (see above)
+IN.Ncomp  = 8;       % Number of species tracked
+
+% Max number of eruptions
+IN.nErupt = 1e4;
+
+%%%%%%%%%%%%%%%%%%
+% Run Model
+%%%%%%%%%%%%%%%%%%
+run('../../main.m');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
